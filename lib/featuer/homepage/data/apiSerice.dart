1f7fba375f4daec79956ec299d80ec75a93f80d4
yahya mosa <yhea23112003@gamil.com>
2025-09-02 14:59:58 +0300

    finsh the api service class

--- lib/featuer/homepage/data/apiSerice.dart
+++ lib/featuer/homepage/data/apiSerice.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 class Apiservice {
   Apiservice({required this.dio});
   final Dio dio;
+
   Future<List<dynamic>> getData() async {
     var respons = await dio.get("https://fakestoreapi.com/products");
     if (respons.statusCode == 200) {
@@ -10,6 +11,43 @@ class Apiservice {
       return respons.data;
     } else {
       throw Exception('Failed to load data');
+    }
+  }
+
+  // POST
+  Future<Map<String, dynamic>> postData(Map<String, dynamic> data) async {
+    final response = await dio.post(
+      "https://fakestoreapi.com/products",
+      data: data,
+    );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response.data;
+    } else {
+      throw Exception('Failed to post data');
+    }
+  }
+
+  // PUT
+  Future<Map<String, dynamic>> updateData(
+    int id,
+    Map<String, dynamic> data,
+  ) async {
+    final response = await dio.put(
+      "https://fakestoreapi.com/products/$id",
+      data: data,
+    );
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception('Failed to update data');
+    }
+  }
+
+  // DELETE
+  Future<void> deleteData(int id) async {
+    final response = await dio.delete("https://fakestoreapi.com/products/$id");
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete data');
     }
   }
 }
